7b074494f17a737545e25c3de12345a20b2d1fac
ELOTECH\victor.macedo <victor.macedo@elotech.com.br>
2021-02-19 13:21:03 -0300

    Adicionado rota pelo activemq

--- src/main/java/br/com/camel/routes/RotaPedidoXmlFileToJsonHttpPost.java
+++ src/main/java/br/com/camel/routes/RotaPedidoXmlFileToJsonHttpPost.java
@@ -13,17 +13,17 @@ public class RotaPedidoXmlFileToJsonHttpPost extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        JacksonDataFormat jacksonDataFormat = new JacksonDataFormat();
-        jacksonDataFormat.setPrettyPrint(true);
-        from("file://pedidos?noop=true").
-                log("Lendo Arquivo XML ${file:name}").
-                split(xpath("/pedido/itens/item")).
-                log("Filtrando arquivos EBOOK").
-                filter(xpath("/item/formato/text()='EBOOK'")).
-                log("Convertendo para JSON").
-                unmarshal(new JaxbDataFormat(JAXBContext.newInstance(Item.class))).
-                marshal(jacksonDataFormat).
-                log("Enviando request post to http://localhost:8080/api").
-                to("http://localhost:8080/api");
+//        JacksonDataFormat jacksonDataFormat = new JacksonDataFormat();
+//        jacksonDataFormat.setPrettyPrint(true);
+//        from("file://pedidos?noop=true").
+//                log("Lendo Arquivo XML ${file:name}").
+//                split(xpath("/pedido/itens/item")).
+//                log("Filtrando arquivos EBOOK").
+//                filter(xpath("/item/formato/text()='EBOOK'")).
+//                log("Convertendo para JSON").
+//                unmarshal(new JaxbDataFormat(JAXBContext.newInstance(Item.class))).
+//                marshal(jacksonDataFormat).
+//                log("Enviando request post to http://localhost:8080/api").
+//                to("http://localhost:8080/api");
     }
 }
